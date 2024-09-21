0731487d2e3ea29930342ba685bf8b6d717ab735
Leo Lark <leo.lark10@gmail.com>
2024-09-21 22:51:46 +1000

    More coffee fields

--- javagqldemo/src/main/java/com/chaourov/javagqldemo/model/Coffee.java
+++ javagqldemo/src/main/java/com/chaourov/javagqldemo/model/Coffee.java
@@ -1,3 +1,3 @@
 package com.chaourov.javagqldemo.model;
 
-public record Coffee(String title) {}
+public record Coffee(String title, String description, String[] ingredients, String image, String id) {}
